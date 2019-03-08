37c44ef1c3d93400b096abdfa251b9edf3bf4ace
Zihao Chen <chen.zihao@microsoft.com>
2019-03-08 13:53:45 +0800

    Read settings from config and write to seq server

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Serilog;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration().ReadFrom.AppSettings().CreateLogger();
+
+            Serilog.Debugging.SelfLog.Enable(Console.Out);
+            Log.Logger = log;
+            Trace.TraceInformation("Tracing test");
+            Console.ReadLine();
+            Log.CloseAndFlush();
+        }
+    }
+}
